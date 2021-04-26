3c47bb4f80b059d578ab5f7778a368e96a61d285
gixnluca <gianlucademarchi9@gmail.com>
2021-04-26 23:25:34 +0200

    Indentation fix

--- payment-failed-dm/app/main.py
+++ payment-failed-dm/app/main.py
@@ -18,53 +18,55 @@ HYPER_SECRET_KEY = os.environ.get('HYPER_SECRET_KEY')
 DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 DISCORD_CHANNEL_ID = os.environ.get('DISCORD_CHANNEL_ID')
 
+
 @app.route('/', methods=['POST'])
 def stripe_endpoint():
-	body = request.data.decode('utf-8')
-	signature_header = request.headers.get('Stripe-Signature', None)
+    body = request.data.decode('utf-8')
+    signature_header = request.headers.get('Stripe-Signature', None)
 
-	try:
-		event = stripe.Webhook.construct_event(
-			body, signature_header, STRIPE_WEBHOOK_SECRET
-		)
+    try:
+        event = stripe.Webhook.construct_event(
+            body, signature_header, STRIPE_WEBHOOK_SECRET
+        )
 
-		if event['type'] == 'charge.failed':
-			customer_id = event['data']['object']['customer']
+        if event['type'] == 'charge.failed':
+            customer_id = event['data']['object']['customer']
 
-			headers = {
-				'Authorization': f'Bearer {HYPER_SECRET_KEY}'
-			}
+            headers = {
+                'Authorization': f'Bearer {HYPER_SECRET_KEY}'
+            }
 
-			req = requests.get(f'https://api.hyper.co/v4/licenses?customer={customer_id}', headers=headers)
-			if req.status_code == 200:
-				data = req.json()
-				if data['total'] > 0:
-					user = data['data'][0].get('user', {}).get('discord', {})
-					if user:
-						content = '[Payment Failed] '
-						messaged = message_user(
-							token=DISCORD_BOT_TOKEN,
-							user=user['id'],
-							content='Your payment method on file has failed.'
-						)
-						if messaged:
-							content += 'Message sent to '
-						else:
-							content += 'Failed to message '
+            req = requests.get(f'https://api.hyper.co/v4/licenses?customer={customer_id}', headers=headers)
+            if req.status_code == 200:
+                data = req.json()
+                if data['total'] > 0:
+                    user = data['data'][0].get('user', {}).get('discord', {})
+                    if user:
+                        content = '[Payment Failed] '
+                        messaged = message_user(
+                            token=DISCORD_BOT_TOKEN,
+                            user=user['id'],
+                            content='Your payment method on file has failed.'
+                        )
+                        if messaged:
+                            content += 'Message sent to '
+                        else:
+                            content += 'Failed to message '
 
-						message_channel(
-							token=DISCORD_BOT_TOKEN,
-							channel=DISCORD_CHANNEL_ID,
-							content=content + '**{}#{}**'.format(user['username'], user['discriminator'])
-						)
+                        message_channel(
+                            token=DISCORD_BOT_TOKEN,
+                            channel=DISCORD_CHANNEL_ID,
+                            content=content + '**{}#{}**'.format(user['username'], user['discriminator'])
+                        )
 
-		return '', 200
-	except stripe.error.SignatureVerificationError:
-		return 'Bad Request', 400
-	except:
-		traceback.print_exc()
-		return 'Internal Server Error', 500
+        return '', 200
+    except stripe.error.SignatureVerificationError:
+        return 'Bad Request', 400
+    except:
+        traceback.print_exc()
+        return 'Internal Server Error', 500
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return 'Not Found', 404
+    return 'Not Found', 404
--- payment-failed-dm/app/utils.py
+++ payment-failed-dm/app/utils.py
@@ -2,43 +2,45 @@ import requests
 
 BASE_URL = 'https://discordapp.com'
 
+
 def message_user(token, user, content):
-	'''Send a direct message to the user'''
-	headers = {
-		'Authorization': f'Bot {token}',
-		'Content-Type': 'application/json'
-	}
+    """Send a direct message to the user"""
+    headers = {
+        'Authorization': f'Bot {token}',
+        'Content-Type': 'application/json'
+    }
 
-	req = requests.post(
-		BASE_URL + '/api/users/@me/channels',
-		headers=headers,
-		json={'recipient_id': user}
-	)
-	if req.status_code == 200:
-		message_req = requests.post(
-			BASE_URL + '/api/channels/{}/messages'.format(req.json()['id']),
-			headers=headers,
-			json={'content': content}
-		)
+    req = requests.post(
+        BASE_URL + '/api/users/@me/channels',
+        headers=headers,
+        json={'recipient_id': user}
+    )
+    if req.status_code == 200:
+        message_req = requests.post(
+            BASE_URL + '/api/channels/{}/messages'.format(req.json()['id']),
+            headers=headers,
+            json={'content': content}
+        )
 
-		if message_req.status_code == 200:
-			return True
+        if message_req.status_code == 200:
+            return True
 
-	return None
+    return None
+
 
 def message_channel(token, channel, content):
-	'''Send a message in a channel'''
-	headers = {
-		'Authorization': f'Bot {token}',
-		'Content-Type': 'application/json'
-	}
+    """Send a message in a channel"""
+    headers = {
+        'Authorization': f'Bot {token}',
+        'Content-Type': 'application/json'
+    }
 
-	req = requests.post(
-		BASE_URL + f'/api/channels/{channel}/messages',
-		headers=headers,
-		json={'content': content}
-	)
-	if req.status_code == 200:
-		return True
+    req = requests.post(
+        BASE_URL + f'/api/channels/{channel}/messages',
+        headers=headers,
+        json={'content': content}
+    )
+    if req.status_code == 200:
+        return True
 
-	return None
+    return None
