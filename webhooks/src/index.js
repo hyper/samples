322ccc185a4c9ef4a48cb6e3dacb79bb70c7a600
Connor Stevens <conmail1224@gmail.com>
2021-06-14 23:51:35 -0400

    Update webhooks

--- webhooks/src/index.js
+++ webhooks/src/index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
-const { Webhook } = require('diahook');
+const bodyParser = require('body-parser');
+const { Webhook } = require('svix');
 const { handle } = require('./services/webhook');
 
 // Get ENV variables
@@ -13,11 +14,16 @@ const { TOKEN, HOOK } = process.env;
 const PORT = process.env.PORT || 5000;
 
 // Set up server and webhook
-// Optional diahook webhook object to validate request bodies
-// const wh = new Webhook(TOKEN);
+// Diahook webhook object to validate request bodies
+const wh = new Webhook(TOKEN);
 const app = express();
 
 // Set up middleware
+app.use(bodyParser.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  },
+}));
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,9 +31,8 @@ app.use(express.json());
 // POST /
 app.post('/', async (req, res) => {
   try {
-    // Optional payload validation
-    // const payload = wh.verify(req.body, req.headers);
-    await handle(req.body);
+    const payload = wh.verify(req.rawBody, req.headers);
+    await handle(payload);
     res.sendStatus(200);
   } catch (err) {
     res.sendStatus(500);
